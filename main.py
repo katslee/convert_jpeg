765c934f1b30d35a6197d51d573130216821ce6a
kats <lee.kats@gmail.com>
2023-01-04 23:09:55 +0800

    workable

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import os
 import pprint
 import subprocess
 
-src_dir = '/Volumes/Untitled CD'
-dst_dir = '/tmp/cd'
-image = ['bmp', 'jpg', 'jpeg', 'png']
+src_dir = 'F:/1999'
+dst_dir = 'F:/out'
+image = ['.bmp', '.jpg', '.jpeg', '.png']
 
 for root, dirs, files in os.walk(src_dir):
     for f in files:
         prefix, suffix = os.path.splitext(f)
-        if  suffix in image:
+        if suffix.lower() in image:
             abspath_in = root + '/' + f
             dir_out = root.replace(src_dir, dst_dir)
             if not os.path.exists(dir_out):
